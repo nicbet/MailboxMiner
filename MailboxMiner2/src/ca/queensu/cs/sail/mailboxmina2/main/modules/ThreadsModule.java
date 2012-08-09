2b8102af1bf0acfad766df843427df85e45ca326
Nicolas Bettenburg <nicbet@gmail.com>
2012-08-09 16:08:28 -0400

    NB: Added GPLv3+ copyright headers to files.

--- MailboxMiner2/src/ca/queensu/cs/sail/mailboxmina2/main/modules/ThreadsModule.java
+++ MailboxMiner2/src/ca/queensu/cs/sail/mailboxmina2/main/modules/ThreadsModule.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Nicolas Bettenburg.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Nicolas Bettenburg - initial API and implementation
+ ******************************************************************************/
 package ca.queensu.cs.sail.mailboxmina2.main.modules;
 
 import java.sql.Connection;
